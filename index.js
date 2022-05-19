f21fb6fe735ca4f8ea2e479e71c579aec68d906a
Alan Lopes Melo <allanlopesmelo@gmail.com>
2022-05-18 22:16:39 -0300

    Add template engine e tela de login e cadastro

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
-import { fileURLToPath } from "url";
 import path from "path";
+import { fileURLToPath } from "url";
+
 
 
 const server = express();
@@ -11,7 +12,14 @@ const __dirname = path.dirname(__filename);
 server.use(express.urlencoded({ extended: true })); //habilita o uso do post dentro das rotas
 server.use(express.static(path.join(__dirname + "/public"))); //habilita o uso de arquivos estaticos
 server.set("views", path.join(__dirname + "/src/views")); //define a pasta de views
+server.set("view engine","vash");
 
+server.get('/', (req, res) => {
+
+	let test = 'TESTANDO'
+
+	res.render('login', {test});
+});
 
 server.listen(3000, () => {
 	console.log(`Server is running on port 3000`);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-
-server.get("/", async (req, res) => {
-	console.log("bateu");
-});
