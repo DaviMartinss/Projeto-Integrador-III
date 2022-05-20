a5f8a76b41a4c1b37cfac6e19557aaa75f762a9c
Alan Lopes Melo <allanlopesmelo@gmail.com>
2022-05-20 18:50:12 -0300

    Adicionando arquivo db.js novamente

---
+++ repository/db.js
@@ -0,0 +1,36 @@
+import pkg from 'pg';
+const { Pool } = pkg;
+
+
+//const { Client } = require('pg');
+
+class Database{
+
+  async connect() {
+      if (global.connection)
+          return global.connection.connect();
+
+      const pool = new Pool({
+          user: 'seu user',
+          host: '',
+          database: 'bd_pedemeia',
+          password: 'sua senha',
+          port: 5432,
+      });
+
+      //apenas testando a conexão
+      const user = await pool.connect();
+      console.log("Criou pool de conexões no PostgreSQL!");
+
+      const res = await user.query('SELECT NOW()');
+      console.log(res.rows[0]);
+      user.release();
+
+      //guardando para usar sempre o mesmo
+      global.connection = pool;
+      return pool.connect();
+  }
+}
+
+export const database = new Database();
+//module.exports = { selectUser }
