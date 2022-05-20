d770cf3ec482714f8904513e8ef2af0c763e3f8d
Davi Martins <davimartins.dgcm@gmail.com>
2022-05-19 21:16:27 -0300

    Modificação de coluna nas consultas de select e insert

--- repository/UserRepository.js
+++ repository/UserRepository.js
@@ -4,13 +4,13 @@ class UserRepository {
   async selectUser(email, password) {
       const bd = await database.connect();
       //console.log("select * from" + ' "user" '+ " WHERE login='" + email +"' AND senha='" + password + "';");
-      const res = await bd.query("select * from" + ' "user" '+ " WHERE login='" + email +"' AND senha='" + password + "';");
+      const res = await bd.query("select * from" + ' "user" '+ " WHERE Email='" + email +"' AND password='" + password + "';");
       return res.rows;
   }
 
      async insertUser(user){
        const db = await database.connect();
-      const sql = 'INSERT INTO "user" (nome, login, senha) VALUES ($1,$2,$3);';
+      const sql = 'INSERT INTO "user" (nickname, Email, password) VALUES ($1,$2,$3);';
        const values = [user.logname, user.logemail, user.logpass];
        await db.query(sql, values);
    }
