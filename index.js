7c6aa5095d3f86b9cb70490aab6a287a9eb2e453
Alan Lopes Melo <allanlopesmelo@gmail.com>
2022-05-22 11:31:46 -0300

    Atualizado criptografia e criado JSONs para user

--- index.js
+++ index.js
@@ -4,9 +4,14 @@ import path from "path";
 import { fileURLToPath } from "url";
 import aes256 from "aes256";
 
+//Encriptografia
 var key = 'bf3c199c2470cb477d907b1e0917c17b';
+var cipher = aes256.createCipher(key);
+//Ex:
+	//	cipher.encrypt(req.body.logpass)
+	//	var decryptedSenha = cipher.decrypt("senha");
 
-//var decryptedPlainText = cipher.decrypt(encryptedPlainText);
+
 //Repository IMPORTS
 import { database } from "./repository/db.js";
 import { userRepository } from "./repository/UserRepository.js";
@@ -18,7 +23,7 @@ const server = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-var emailUser = "";
+var userId;
 
 server.use(express.json())
 server.use(express.urlencoded({ extended: true })); //habilita o uso do post dentro das rotas
@@ -35,58 +40,117 @@ server.get('/', (req, res) => {
 });
 
 server.post("/", async (req, res) => {
-	//verificar se o usuario é válido
-	var userData = {email:req.body.logemail, password: btoa(req.body.logpass)}
-	emailUser = userData.email; //atribui para a variavél global emailUser o email do usuário
-	var getUser = await userRepository.selectUser(userData);
 
-	var decryptedSenha = cipher.decrypt("senha");
-	
-	if (getUser[0] != undefined) {
+	//verificar se o usuario é válido
+	var userData = {email:req.body.logemail, password: cipher.encrypt(req.body.logpass)}
+
+	var getUser = await userRepository.getUser(userData);
+
+	if (getUser != undefined) {
+		userId = getUser.id_user;
 		res.render("index", { erroLogin: false });
 	} else {
 		res.render("login", { erroLogin: true });
 	}
 });
 
+//verifica usuários no banco , essa rota e para auxilio de testes
+server.get("/usuarioList", async (req, res) => {
+
+	var getUserList = await userRepository.getUserList();
+
+	res.send(getUserList);
+
+});
+
 //cadastra usuário
-server.post("/user_cadastro", async (req, res) => {
+server.post("/usuario", async (req, res) => {
 
 	//verificar se o email já foi cadastrado
-	var userExiste = await userRepository.getUserByEmail(req.body.logemail);
+	var userExiste = await userRepository.getUserByEmail(req.body.Email);
+
+	//verifica se o insert ocorreu com sucesso!
+	var insertUser;
 
 	if(userExiste == ''){
 
 		//var buffer = Buffer.from(req.body.logpass);
-		var cipher = aes256.createCipher(key);
-		req.body.logpass = cipher.encrypt(req.body.logpass);
-		console.log("senha crip: "+req.body.logpass);
 
-		await userRepository.insertUser(req.body);
-		sendMailBemVindo.run(req.body.logname, req.body.logemail);
+		//req.body.logpass = cipher.encrypt(req.body.logpass);
+		//console.log("senha crip: "+req.body.logpass);
+
+		var userData = req.body
+
+		userData.Password = cipher.encrypt(userData.Password); //criptografia md5
+
+		insertUser = await userRepository.insertUser(userData);
+
+		if(insertUser)
+		{
+			sendMailBemVindo.run(userData.NickName, userData.Email);
+			res.redirect("/");
+		}
+		else
+		{
+			console.log("ERRO NO CADASTRO DO USUÁRIO");
+			res.render("login", { erroLogin: true });
+		}
+
 	}else{
-		console.log("Erro");
+		console.log("Email já foi cadastrado por outro usuário!");
 	}
-
-	res.redirect("/");
 });
 
 //atualiza usuário
-server.put("/user_update", async (req, res) => {
+server.put("/usuario", async (req, res) => {
 
-	//atualizar dados do usuário (passo o email porque ele deve ser único)
-	await userRepository.updateUser(req.body, emailUser);
-	res.redirect("/"); //deve ser redirecionanda para a tela de dados do usuário
+	var userData = req.body
+
+	//Descomentar quando tiver o front funcionando para o usuário logar
+	//userData.UserId = userId
+
+	userData.Password = cipher.encrypt(userData.Password); //criptografia md5
+
+	//verifica se o update ocorreu com sucesso!
+	var updateUser;
+
+	updateUser = await userRepository.updateUser(userData);
+
+	if(updateUser)
+	{
+		//deve redirecionar para página de informações do usuário
+		console.log("USUÁRIO ATUALIZADO");
+	}
+	else
+	{
+		//deve redirecionar para página de informações do usuário com o alerta ERRO
+		console.log("ERRO NA ATUALIZAÇÃO");
+	}
 
 });
 
 //deleta usuário
-server.delete("/user_delete", async (req, res) => {
+server.delete("/usuario", async (req, res) => {
+
+	//Comentar esse código abaixo quando front tiver funcional
+	userId = req.body.UserId;
+
+	//verifica se o delete ocorreu com sucesso!
+	var deleteUser;
 
 	//deletamos o usuário pelo o id
-	var user = await userRepository.getUserByEmail(emailUser);
-	await userRepository.deleteUser(user.id_user);
-	res.redirect("/"); //deve ser redirecionanda para a tela de dados do usuário
+	deleteUser = await userRepository.deleteUser(userId);
+
+	if(deleteUser)
+	{
+		console.log("USUÁRIO DELETADO");
+		res.redirect("/");
+	}
+	else
+	{
+		//deve redirecionar para página de informações do usuário com o alerta ERRO
+		console.log("ERRO AO DELETAR O USUÁRIO");
+	}
 
 });
 // ========================== CRUD DE CATEGORIA =====================================================================
@@ -100,8 +164,8 @@ server.get("/categoria", async (req, res) => {
 
 //cadastro da categoria
 server.post("/categoria", async (req, res) => {
-	
-	var user = await userRepository.getUserByEmail(emailUser);
+
+	var user = await userRepository.getUser(emailUser);
 	//Em req.body tem que passar o nome da categoria
 	await categoriaRepository.insertCategoria(req.body, user.id_user); //cadastrando categoria
 
@@ -109,7 +173,7 @@ server.post("/categoria", async (req, res) => {
 
 //atualiza categoria
 server.put("/categoria_update", async (req, res) => {
-	
+
 	var res = await categoriaRepository.updateCategoria(req.body, "categoria antiga"); // passa o nome da categoria a ser deletada
 	console.log(res);
 
@@ -117,11 +181,12 @@ server.put("/categoria_update", async (req, res) => {
 
 //deleta categoria
 server.delete("/categoria_delete", async (req, res) => {
-	
+
 	var res = await categoriaRepository.deleteCategoria(req.body); // passa o nome da categoria a ser deletada
 	console.log(res);
 
 });
+
 // ========================== CRUD DE CARTÕES =====================================================================
 
 server.get("/cartaoList", async (req, res) => {
@@ -166,20 +231,30 @@ server.post("/cartao", async (req, res) => {
 		//INSERT Cartão de Crédito
 		insertCartao = await cartaoCreditoRepository.insertCartao(cartaoData);
 
+		if(insertCartao)
+		{
+			//redenrizar TELA DE CARTAO DE CREDITO
+			console.log("CADASTROU");
+		}else
+		{
+			//redenrizar TELA DE CADASTRO DE CARTAO CREDITO COM ALERTA DE ERRO
+			console.log("ERRO NO CADASTRO");
+		}
+
 	}else
 	{
 		//INSERT Cartão de Débito
 		insertCartao = await cartaoDebitoRepository.insertCartao(cartaoData);
-	}
 
-	if(insertCartao)
-	{
-		//redenrizar TELA DE CARTAO DE CREDITO
-		console.log("CADASTROU");
-	}else
-	{
-		//redenrizar TELA DE CADASTRO DE CARTAO COM ALERTA DE ERRO
-		console.log("ERRO NO CADASTRO");
+		if(insertCartao)
+		{
+			//redenrizar TELA DE CARTAO DE DEBITO
+			console.log("CADASTROU");
+		}else
+		{
+			//redenrizar TELA DE CADASTRO DE CARTAO DEBITO COM ALERTA DE ERRO
+			console.log("ERRO NO CADASTRO");
+		}
 	}
 });
 
@@ -202,21 +277,33 @@ server.put("/cartao", async (req, res) => {
 		//UPDATE Cartão de Crédito
 		updateCartao = await cartaoCreditoRepository.updateCartao(cartaoData);
 
+		if(updateCartao)
+		{
+			//redenrizar TELA DE CARTAO DE CREDITO
+			console.log("ATUALIZOU");
+		}else
+		{
+			//redenrizar TELA DE UPDATE DE CARTAO CREDITO COM ALERTA DE ERRO
+			console.log("ERRO NA ATUALIZAÇÃO");
+		}
+
 	}else
 	{
 		//UPDATE Cartão de Débito
 		updateCartao = await cartaoDebitoRepository.updateCartao(cartaoData);
+
+		if(updateCartao)
+		{
+			//redenrizar TELA DE CARTAO DE DEBITO
+			console.log("ATUALIZOU");
+		}else
+		{
+			//redenrizar TELA DE UPDATE DE CARTAO DEBITO COM ALERTA DE ERRO
+			console.log("ERRO NA ATUALIZAÇÃO");
+		}
 	}
 
-	if(updateCartao)
-	{
-		//redenrizar TELA DE CARTAO DE CREDITO
-		console.log("ATUALIZOU");
-	}else
-	{
-		//redenrizar TELA DE UPDATE DE CARTAO COM ALERTA DE ERRO
-		console.log("ERRO NA ATUALIZAÇÃO");
-	}
+
 });
 
 server.delete("/cartao", async (req, res) => {
@@ -237,20 +324,30 @@ server.delete("/cartao", async (req, res) => {
 		//DELETE Cartão de Crédito
 		deleteCartao = await cartaoCreditoRepository.deleteCartao(cartaoData);
 
+		if(deleteCartao)
+		{
+			//redenrizar TELA DE CARTAO DE CREDITO
+			console.log("APAGOU CARTAO");
+		}else
+		{
+			//redenrizar TELA DE CARTAO CREDITO COM ALERTA DE ERRO
+			console.log("ERRO AO APAGAR CARTAO");
+		}
+
 	}else
 	{
 		//DELETE Cartão de Débito
 		deleteCartao = await cartaoDebitoRepository.deleteCartao(cartaoData);
-	}
 
-	if(deleteCartao)
-	{
-		//redenrizar TELA DE CARTAO DE CREDITO
-		console.log("APAGOU CARTAO");
-	}else
-	{
-		//redenrizar TELA DE CARTAO COM ALERTA DE ERRO
-		console.log("ERRO AO APAGAR CARTAO");
+		if(deleteCartao)
+		{
+			//redenrizar TELA DE CARTAO DE DEBITO
+			console.log("APAGOU CARTAO");
+		}else
+		{
+			//redenrizar TELA DE CARTAO DEBITO COM ALERTA DE ERRO
+			console.log("ERRO AO APAGAR CARTAO");
+		}
 	}
 });
 
--- repository/UserRepository.js
+++ repository/UserRepository.js
@@ -1,46 +1,168 @@
 import { database } from "./db.js";
+
 class UserRepository {
 
-  async selectUser(user) {
-      const bd = await database.connect();
-      const sql = 'select * from "user" WHERE Email=$1 AND password=$2;';
-      const values = [user.email, user.password]
-      const res = await bd.query(sql,values);
-      return res.rows;
+  async getUser(user) {
+
+      try {
+
+        const db = await database.connect();
+
+        if(db != undefined )
+        {
+          const sql = 'select * from "user" WHERE Email=$1 AND password=$2;';
+          const values = [user.email, user.password]
+          const user = await db.query(sql,values);
+          return user;
+        }
+        else
+        {
+          console.log("ERRO NA CONEXÃO COM POSTGREESQL");
+          return undefined;
+        }
+
+      } catch (e) {
+
+        console.log(e);
+        return undefined;
+      }
+  }
+
+  //pega uma lista de usuários
+  async getUserList() {
+
+    try {
+
+      const db = await database.connect();
+
+      if(db != undefined )
+      {
+        const sql = 'select * from "user";';
+        const res = await db.query(sql);
+        return res.rows;
+      }
+      else
+      {
+        console.log("ERRO NA CONEXÃO COM POSTGREESQL");
+        return undefined;
+      }
+
+    } catch (e) {
+
+      console.log(e);
+      return undefined;
+    }
   }
 
   //pega o usuário pelo o email
   async getUserByEmail(email) {
-    const bd = await database.connect();
-    const sql = 'select * from "user" WHERE email=$1;';
-    const res = await bd.query(sql,[email]);
-    return res.rows;
-}
+
+    try {
+
+      const db = await database.connect();
+
+      if(db != undefined )
+      {
+        const sql = 'select * from "user" WHERE email=$1;';
+        const res = await db.query(sql,[email]);
+        return res.rows;
+      }
+      else
+      {
+        console.log("ERRO NA CONEXÃO COM POSTGREESQL");
+        return undefined;
+      }
+
+    } catch (e) {
+
+      console.log(e);
+      return undefined;
+    }
+  }
 
 //cadastra um usuário
   async insertUser(user){
-    const db = await database.connect();
-    const sql = 'INSERT INTO "user" (nickname, Email, password) VALUES ($1,$2,$3);';
-    const values = [user.logname, user.logemail, user.logpass];
-    await db.query(sql, values);
-   }
+
+    try {
+
+      const db = await database.connect();
+
+      if(db != undefined)
+      {
+        const sql = 'INSERT INTO "user" (nickname, Email, password) VALUES ($1,$2,$3);';
+        const values = [user.NickName, user.Email, user.Password];
+        await db.query(sql, values);
+
+        return true;
+      }
+      else
+      {
+        console.log("ERRO NA CONEXÃO COM POSTGREESQL");
+        return false;
+      }
+
+    } catch (ex) {
+
+      console.log(ex);
+      return false;
+    }
+  }
 
    //update do usuário
-   async updateUser(user, email){
-    const db = await database.connect();
-    const sql = 'UPDATE "user" SET nickname=$1, Email=$2, password=$3 WHERE Email=$4';
-    const values = [user.logname, user.logemail, user.logpass, email];
-    await db.query(sql, values);
+   async updateUser(user){
+
+     try {
+
+       const db = await database.connect();
+
+       if(db != undefined)
+       {
+         const sql = 'UPDATE "user" SET nickname=$1, Email=$2, password=$3 WHERE id_user=$4';
+         const values = [user.NickName, user.Email, user.Password, user.UserId];
+         await db.query(sql, values);
+
+         return true;
+       }
+       else
+       {
+         console.log("ERRO NA CONEXÃO COM POSTGREESQL");
+         return false;
+       }
+
+     } catch (ex) {
+
+       console.log(ex);
+       return false;
+     }
   }
 
 //deleta usuário
   async deleteUser(user_id){
-    const db = await database.connect();
-    const sql = 'DELETE FROM "user" WHERE id_user=$1';
-    const values = [user_id];
-    await db.query(sql, values);
-  }
 
+    try {
+
+      const db = await database.connect();
+
+      if(db != undefined)
+      {
+        const sql = 'DELETE FROM "user" WHERE id_user=$1';
+        const values = [user_id];
+        await db.query(sql, values);
+
+        return true;
+      }
+      else
+      {
+        console.log("ERRO NA CONEXÃO COM POSTGREESQL");
+        return false;
+      }
+
+    } catch (ex) {
+
+      console.log(ex);
+      return false;
+    }
+  }
 }
 
 export const userRepository = new UserRepository();
