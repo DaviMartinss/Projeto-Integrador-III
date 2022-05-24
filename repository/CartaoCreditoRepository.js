24d378fb007218baa181c4cac9eec79ab1b58fb4
Davi Martins <davimartins.dgcm@gmail.com>
2022-05-24 20:36:43 -0300

    Atualização nas consultas de Cartão de Crédito

--- repository/CartaoCreditoRepository.js
+++ repository/CartaoCreditoRepository.js
@@ -11,7 +11,7 @@ class CartaoCreditoRepository{
 
         if(db != undefined )
         {
-          const sql = 'select * from "cartao_credito" WHERE id_user=$1;';
+          const sql = 'select * from "CartaoCredito" WHERE "UserId"=$1;';
           const values = [userId];
           const res = await db.query(sql,values);
           return res.rows;
@@ -37,15 +37,15 @@ class CartaoCreditoRepository{
 
       if(db != undefined)
       {
-        const sql = 'INSERT INTO "cartao_credito"'
+        const sql = 'INSERT INTO "CartaoCredito"'
                   + '('
-                      + 'id_user,'
-                      + 'num_cartao_credito,'
-                      + 'data_fatura,'
-                      + 'fatura,'
-                      + 'limite,'
-                      + 'anuidade,'
-                      + 'adicional_juro'
+                      + "UserId,"
+                      + "NumCC,"
+                      + "DataFatura,"
+                      + "Fatura,"
+                      + "Limite,"
+                      + "Anuidade,"
+                      + "JurosAdicional"
                   + ')'
                   + 'VALUES ($1,$2,$3,$4,$5,$6,$7);';
         const values = [cartao.UserId,
@@ -80,26 +80,21 @@ class CartaoCreditoRepository{
 
       if(db != undefined)
       {
-        const sql = 'UPDATE "cartao_credito" SET '
-                //  + '('
-                      + 'num_cartao_credito=$1,'
-                      + 'data_fatura=$2,'
-                      + 'fatura=$3,'
-                      + 'limite=$4,'
-                      + 'anuidade=$5,'
-                      + 'adicional_juro=$6'
-                  //+ ')'
-                  +' WHERE id_cartao_credito=$7 AND id_user=$8';
+        const sql = 'UPDATE "CartaoCredito" SET '
+                      + "NumCC=$1,"
+                      + "DataFatura=$2,"
+                      + "Fatura=$3,"
+                      + "Limite=$4,"
+                      + "Anuidade=$5,"
+                      + "JurosAdicional=$6"
+                  +' WHERE "CartaoCreditoId"=$7';
         const values = [cartao.NumCartao,
                         cartao.DataFatura,
                         cartao.Fatura,
                         cartao.Limite,
                         cartao.Anuidade,
                         cartao.JurosAdicional,
-                        cartao.CartaoCreditoId,
-                        cartao.UserId];
-
-        //console.log(sql);
+                        cartao.CartaoCreditoId];
 
         await db.query(sql, values);
 
@@ -126,8 +121,8 @@ class CartaoCreditoRepository{
 
       if(db != undefined)
       {
-        const sql = 'DELETE FROM "cartao_credito" WHERE id_cartao_credito=$1 AND id_user=$2';
-        const values = [cartao.CartaoCreditoId, cartao.UserId];
+        const sql = 'DELETE FROM "CartaoCredito" WHERE "CartaoCreditoId"=$1';
+        const values = [cartao.CartaoCreditoId];
         await db.query(sql, values);
 
         return true;
