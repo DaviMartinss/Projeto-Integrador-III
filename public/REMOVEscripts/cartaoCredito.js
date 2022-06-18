3f36de452c929a4d828ac4ab47085780c4f1e9e6
Marlon Duarte <eletromarlon@gmail.com>
2022-06-17 21:32:01 -0300

    Removing legacy folders

--- public/REMOVEscripts/cartaoCredito.js
+++
@@ -1,7 +0,0 @@
-function confirmar(numCC){
-    var alert = window.confirm("Tem certeza que deseja Apagar o Cartão de Crédito de número "+numCC+ " ?");
-    if(alert){
-        window.location.href = "http://localhost:3000/deleteCartao?NumCartao="+numCC+"&Type=CC";
-    }
-  
-}
--- public/scripts/cartaoDebito.js
+++
@@ -1,7 +0,0 @@
-function confirmar(numCD){
-    var alert = window.confirm("Tem certeza que deseja Apagar o Cartão de Débito de número "+numCD+ " ?");
-    if(alert){
-        window.location.href = "http://localhost:3000/deleteCartao?NumCartao="+numCD+"&Type=CD";
-    }
-  
-}
