b4795682513dca498bfb3cc4a2d5d98cef2a93ea
Alan Lopes Melo <allanlopesmelo@gmail.com>
2022-06-22 21:40:14 -0300

    Adicionando metodos novamente de categoria

--- public/scripts/categoria.js
+++ public/scripts/categoria.js
@@ -1,19 +1,50 @@
 function confirmar(categoria){
-    console.log("A categoria é "+categoria);   
+    console.log("A categoria é "+categoria);
     var alert = window.confirm("Tem certeza que deseja Apagar " + categoria.Categoria +" da sua lista de Categorias?");
     if(alert){
         window.location.href = "http://localhost:3000/categoriaDEL?CategoriaId="+categoria.CategoriaId;
     }
 }
 
-//código do alan
-{/* <div id="atualiza" style="display:none">
-<form action="/categoriaUP" method="post">
-   <input type="text" id="categoriaId" name="CategoriaId" hidden/>
-   <label>Categoria:</label>
-   <input type="text" id="categoria" name="Categoria" />
-   <button type="submit" id="btAtualizar" class="btn app-btn-secondary" style="display:none">
-   Atualizar
-   </button>
-</form>
-</div> */}
+function atualizar(categoriaDATA){
+
+  var categoria = categoriaDATA;
+
+  var div = document.getElementById("atualiza");
+  var inputCategoria = document.getElementById("categoria");
+  var inputCategoriaId = document.getElementById("categoriaId");
+  var butaoAtualiza = document.getElementById("btAtualizar");
+
+  if(div.style.display == "none")
+  {
+    div.style.display = 'block';
+    butaoAtualiza.style.display = 'block';
+    inputCategoria.value = categoria.Categoria;
+    inputCategoriaId.value = categoria.CategoriaId;
+  }
+  else
+  {
+    div.style.display = 'none';
+    butaoAtualiza.style.display = 'none';
+    inputCategoria.value = "";
+    inputCategoriaId.value = "";
+  }
+
+  function cadastrar(){
+
+  var div = document.getElementById("cadastro");
+  var butaoCadastrar = document.getElementById("btCadastrar");
+
+  if(div.style.display == "none")
+      div.style.display = 'block';
+  {
+    div.style.display = 'block';
+    butaoCadastrar.style.display = 'block';
+  }
+  else
+      div.style.display = 'none';
+  {
+    div.style.display = 'none';
+    butaoCadastrar.style.display = 'none';
+  }
+}
